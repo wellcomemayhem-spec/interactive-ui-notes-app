5798914f80f2fdc08a3ea661f62154940356c2d5
wellcomemayhem-spec <hirajfaryar@hotmail.com>
2025-08-15 11:51:35 +0800

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the Interactive Notes App</h1>
+      {/* Add components for notes, editing, etc. here */}
+    </div>
+  );
+}
+
+export default App;
