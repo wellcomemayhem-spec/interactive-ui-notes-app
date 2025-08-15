1e2846b6832e701dd5667c4bccba6a5de7c6f259
wellcomemayhem-spec <hirajfaryar@hotmail.com>
2025-08-15 11:51:38 +0800

    feat: initial commit for src/components/Notes.js

---
+++ src/components/Notes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Notes = () => {
+  return (
+    <div>
+      <h2>Your Notes</h2>
+      {/* Add logic for displaying notes here */}
+    </div>
+  );
+};
+
+export default Notes;
